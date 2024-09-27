8dc4c51ab384f4ded5fea0455d5add4d38b8e206
Ehab <ihapbpc@gmail.com>
2024-09-27 16:37:36 +0200

    adding 9090

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn portfolio_mgmnt.wsgi:application
+web: gunicorn portfolio_mgmnt.wsgi:application --bind 0.0.0.0:9090
--- portfolio_mgmnt/settings.py
+++ portfolio_mgmnt/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'portfolio_mgmnt.urls'
