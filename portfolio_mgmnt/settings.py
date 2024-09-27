a142ae0986760209b1575b22b6892f39d0a39830
Ehab <ihapbpc@gmail.com>
2024-09-27 17:48:34 +0200

    allowing all hosts

--- portfolio_mgmnt/settings.py
+++ portfolio_mgmnt/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5x5sdd@^19!r3lx&-wzuqicasu^*mjr#a(xy%+#a)pe112&y)2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
