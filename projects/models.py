4f4391f4805dae64f4bccfbdce0a06b49f9dc861
Ehab <ihapbpc@gmail.com>
2024-10-27 14:51:10 +0200

    added deployment links

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 class Project(models.Model):
     title = models.CharField(max_length=266)
     descriptions = models.TextField(max_length=500)
     difficulties = models.CharField(max_length=266, blank=True, null=True)
     solution = models.CharField(max_length=266, blank=True, null=True)
     link = models.CharField(max_length=266, blank=True, null=True)
+    deployment_link = models.URLField(max_length=266, blank=True, null=True)  # New field for deployment link
     image = models.ImageField(upload_to='projects/%Y/%m/%d/', blank=True, null=True)
     
     def __str__(self):
